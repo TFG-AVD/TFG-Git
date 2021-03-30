49bf40f7f61e3d3fcb7b93de50ea66a668bb70c0
TFG-AVD <tfgifp@gmail.com>
2021-03-30 17:16:34 +0200

    SplashScreen TERMINADO

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.tfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,4000);
 
     }
-    //DANI
 
-    //ALEX imbeecil eieieiei
 }

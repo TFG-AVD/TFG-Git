a4fc8fb86db83bca15fe96366d2153e13ceedb3b
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-03-30 17:28:56 +0200

    Cambios en MainActivity (video 5)

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Button joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        Button loginButton = (Button) findViewById(R.id.main_join_btn);
 
         // PARA EL SPLASH SCREEN
         TimerTask tarea = new TimerTask() {
@@ -30,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         Timer tiempo = new Timer();
         tiempo.schedule(tarea,4000);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        
     }
+
+    
 
 }

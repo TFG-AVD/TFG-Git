3df452e7327e0c9db5bbb9224401ce5bff88d148
vblan <viblancom@gmail.com>
2021-04-01 12:27:50 +0200

    LoginActivity + clase usuario

--- app/src/main/java/com/example/tfg/LoginActivity.java
+++ app/src/main/java/com/example/tfg/LoginActivity.java
@@ -1,14 +1,100 @@
 package com.example.tfg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.tfg.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private AlertDialog loadingBar;
+
+    private String parentDbName = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPassword = (EditText) findViewById(R.id.login_password);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_telefono);
+        loadingBar = new AlertDialog(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LoginUser();
+            }
+        });
+
+    }
+
+    private void LoginUser(){
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "please write your phone number", Toast.LENGTH_LONG).show();
+
+        }else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "please write your password", Toast.LENGTH_LONG).show();
+
+        }else{
+            loadingBar.setTitle("Login account");
+            loadingBar.setMessage("Please wait, checking credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(String phone, String password){
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(parentDbName).child(phone).exists()) {
+                    Users userData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
+                    if (userData.getPhone().equals(phone)) {
+                        if (userData.getPassword().equals(password)) {
+                            Toast.makeText(LoginActivity.this, "Logged in succesfully", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Account with this " + phone + " number do not exists", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tfg/dominio/Prevalent.java
+++ app/src/main/java/com/example/tfg/dominio/Prevalent.java
@@ -1,8 +1,10 @@
 package com.example.tfg.dominio;
 
+import com.example.tfg.Model.Users;
+
 public class Prevalent {
 
-  //  private static Users usuarioOnline;
+    private static Users usuarioOnline;
     private static final String userKey ="UserKey";
     private static final String passKey ="UserPass";
 }

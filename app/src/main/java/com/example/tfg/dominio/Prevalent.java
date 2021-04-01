51d95913ccde7ed87d8d4e4387deb3044458e8be
TFG-AVD <tfgifp@gmail.com>
2021-04-01 12:09:35 +0200

    prevalent

---
+++ app/src/main/java/com/example/tfg/dominio/Prevalent.java
@@ -0,0 +1,8 @@
+package com.example.tfg.dominio;
+
+public class Prevalent {
+
+  //  private static Users usuarioOnline;
+    private static final String userKey ="UserKey";
+    private static final String passKey ="UserPass";
+}

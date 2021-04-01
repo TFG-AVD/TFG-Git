a04393a6fb7b5ef28acf6dea9c74720f2ab3e79b
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-04-01 12:11:19 +0200

    RegisterActivity acabado

--- app/src/main/java/com/example/tfg/RegisterActivity.java
+++ app/src/main/java/com/example/tfg/RegisterActivity.java
@@ -4,18 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -83,7 +89,45 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                if (!(dataSnapshot.child("Users").child(phone).exists()))
+                {
+                    HashMap<String, Object> userdataMap = new HashMap<>(),
+                    userdataMap.put("phone", phone);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", name);
 
+                    RootRef.child("Users").child(phone).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if(task.isSuccessful())
+                                    {
+                                        Toast.makeText(RegisterActivity.this, "Congratulations, your accound has been created.", Toast.LENGTH_SHORT);
+                                        loadingbar.dismiss();
+
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else
+                                    {
+                                        loadingbar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Network Error: Please try again some time...", Toast.LENGTH_SHORT);
+                                    }
+                                }
+
+                            });
+
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "This " + phone + " already exist", Toast.LENGTH_SHORT);
+                    loadingbar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please try again using another phone number", Toast.LENGTH_SHORT);
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
 
             @Override

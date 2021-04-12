ad557b3030eae878c967c8626dd071a3991cc422
vblan <viblancom@gmail.com>
2021-04-12 18:46:38 +0200

    Home activity añadido usuario actual+

--- app/src/main/java/com/example/tfg/dominio/HomeActivity.java
+++ app/src/main/java/com/example/tfg/dominio/HomeActivity.java
@@ -44,7 +44,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null).show();// tu madre la gorda
             }
         });
 

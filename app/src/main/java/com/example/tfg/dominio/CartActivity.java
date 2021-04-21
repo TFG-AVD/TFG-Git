9b9162f91e9692c6783cb536ef44abe023ce4cee
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-04-21 18:58:34 +0200

    CartActivity

--- app/src/main/java/com/example/tfg/dominio/CartActivity.java
+++ app/src/main/java/com/example/tfg/dominio/CartActivity.java
@@ -1,22 +1,29 @@
 package com.example.tfg.dominio;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tfg.Model.Cart;
+import com.example.tfg.ProductDetailsActivity;
 import com.example.tfg.R;
 import com.example.tfg.ViewHolder.CartViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -59,6 +66,46 @@ public class CartActivity extends AppCompatActivity {
                 cartViewHolder.txtProductPrice.setText("Precio = " + cart.getPrice());
                 cartViewHolder.txtProductName.setText(cart.getPname());
 
+                cartViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CharSequence options[] = new CharSequence[]
+                                {
+                                        "Editar",
+                                        "Borrar"
+                                };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(CartActivity.this);
+                        builder.setTitle("Opciones de carrito");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if (i == 0){
+                                    Intent intent = new Intent(CartActivity.this, ProductDetailsActivity.class);
+                                    intent.putExtra("pid", cart.getPid());
+                                    startActivity(intent);
+                                }
+                                if (i == 1){
+                                    cartListRef.child("User View").child(Prevalent.usuarioOnline.getPhone())
+                                            .child("Products")
+                                            .child(cart.getPid())
+                                            .removeValue()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()){
+                                                        Toast.makeText(CartActivity.this, "Producto eliminado con éxito.", Toast.LENGTH_SHORT);
+                                                        Intent intent = new Intent(CartActivity.this, HomeActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
+                        builder.show();
+                    }
+                });
             }
 
             @NonNull

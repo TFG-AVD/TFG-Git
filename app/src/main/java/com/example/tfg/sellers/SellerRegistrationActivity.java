3a9706708de346196d42c6d7a035028652808d97
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-05-19 17:24:48 +0200

    cambios en SellerRegistrationActivity

--- app/src/main/java/com/example/tfg/sellers/SellerRegistrationActivity.java
+++ app/src/main/java/com/example/tfg/sellers/SellerRegistrationActivity.java
@@ -6,20 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tfg.R;
 import com.example.tfg.buyers.MainActivity;
 
 public class SellerRegistrationActivity extends AppCompatActivity {
 
-    private Button sellerLogin;
+    private TextView sellerLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_registration);
 
-        sellerLogin = findViewById(R.id.seller_login);
+        sellerLogin = (TextView)findViewById(R.id.seller_begin);
 
         sellerLogin.setOnClickListener(new View.OnClickListener() {
             @Override

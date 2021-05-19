4c246eb03a1a92596ab4f09dff0767c6706687f6
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-05-19 19:02:29 +0200

    SellerHomeActivity completed

--- app/src/main/java/com/example/tfg/sellers/SellerHomeActivity.java
+++ app/src/main/java/com/example/tfg/sellers/SellerHomeActivity.java
@@ -4,17 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tfg.R;
 import com.example.tfg.admin.SellerProductCategoryActivity;
 import com.example.tfg.buyers.MainActivity;
+import com.example.tfg.models.Products;
+import com.example.tfg.viewholders.ItemViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
 
 public class SellerHomeActivity extends AppCompatActivity {
 
@@ -60,5 +73,69 @@ public class SellerHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_home);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(unverifiedProductsRef.orderByChild("sid").equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid()), Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ItemViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ItemViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ItemViewHolder holder, int position, @NonNull final Products model) {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductStatus.setText("State : "+ model.getProductState());
+                        holder.txtProductPrice.setText("Price = Rp. " + model.getPrice());
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                final String productID = model.getPid();
+
+                                CharSequence options[] = new CharSequence[]{
+                                        "Yes",
+                                        "No"
+                                };
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SellerHomeActivity.this);
+                                builder.setTitle("Do you want to delete this products. Are you sure?");
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int position) {
+                                        if (position == 0){
+                                            deleteProduct(productID);
+                                        }if (position == 1){
+
+                                        }
+                                    }
+                                });
+                                builder.show();
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.seller_item_view, parent, false);
+                        ItemViewHolder holder = new ItemViewHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    private void deleteProduct(String productID) {
+        unverifiedProductsRef.child(productID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(SellerHomeActivity.this, "That item has been delete successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

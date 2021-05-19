650ec764165a1e49a5bd3297df9677f0a01634af
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-05-19 17:53:52 +0200

    SellerHomeActivity created

---
+++ app/src/main/java/com/example/tfg/sellers/SellerHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.tfg.sellers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tfg.R;
+
+public class SellerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_home);
+    }
+}

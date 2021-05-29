f8de95c0e129f7e4b6bdcca52e3a1da05b04d6d9
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-05-29 16:09:06 +0200

    (upgrade) CategoriesActivity

--- app/src/main/java/com/example/tfg/comprador/CategoriesActivity.java
+++ app/src/main/java/com/example/tfg/comprador/CategoriesActivity.java
@@ -2,18 +2,17 @@ package com.example.tfg.comprador;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.tfg.R;
 import com.example.tfg.modelos.Products;
@@ -24,59 +23,68 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
-import io.paperdb.Paper;
-
 public class CategoriesActivity extends AppCompatActivity {
-    private DatabaseReference ProductsRef;
-    private String type = "";
-    private RecyclerView botanicaLista;
-    private Button botanicaBtn;
 
-    @SuppressLint("WrongViewCast")
+    private Button buscarBtn;
+    private EditText inputText;
+    private RecyclerView buscarLista;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>().setQuery(reference.orderByChild("category").equalTo("Botánica"), Products.class).build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
+                holder.txtProductoNombre.setText(model.getPname());
+                holder.txtProductoDescripcion.setText(model.getDescription());
+                holder.txtNombreTienda.setText(model.getSellerName());
+                holder.txtProductoPrecio.setText("Precio: " + model.getPrice() + "€");
+                Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(CategoriesActivity.this, ProductDetailsActivity.class);
+                        intent.putExtra("pid", model.getPid());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_items_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+
+        buscarLista.setAdapter(adapter);
+        adapter.startListening();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_search_products);
 
-/*        botanicaBtn = findViewById(R.id.botanica_foto);
-        botanicaBtn.setOnClickListener(new View.OnClickListener() {
+        inputText = findViewById(R.id.buscar_producto_nombre);
+        buscarBtn = findViewById(R.id.buscar_btn);
+        buscarLista = findViewById(R.id.buscar_lista);
+        buscarLista.setLayoutManager(new LinearLayoutManager(CategoriesActivity.this));
+
+        buscarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+            public void onClick(View view) {
 
-                FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>().setQuery(ProductsRef.orderByChild("category").equalTo("Botánica"), Products.class).build();
-
-                FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
-                        holder.txtProductoNombre.setText(model.getPname());
-                        holder.txtProductoDescripcion.setText(model.getDescription());
-                        holder.txtNombreTienda.setText(model.getSellerName());
-                        holder.txtProductoPrecio.setText("Precio: " + model.getPrice() + "€");
-                        Picasso.get().load(model.getImage()).into(holder.imageView);
-
-                        holder.itemView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Intent intent = new Intent(CategoriesActivity.this, ProductDetailsActivity.class);
-                                intent.putExtra("pid", model.getPid());
-                                startActivity(intent);
-                            }
-                        });
-                    }
-                    @NonNull
-                    @Override
-                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_items_layout, parent, false);
-                        ProductViewHolder holder = new ProductViewHolder(view);
-                        return holder;
-                    }
-                };
-                botanicaLista.setAdapter(adapter);
-                adapter.startListening();
+                onStart();
             }
-        });*/
+        });
     }
 }
-

f53b39e36e1e7b89f9796f015ed4365048aa9de9
AlejandroJiCheungGIT <alejandro.jicheung.git@gmail.com>
2021-05-29 13:00:37 +0200

    (upgrade) CategoriesActivity

--- app/src/main/java/com/example/tfg/comprador/CategoriesActivity.java
+++ app/src/main/java/com/example/tfg/comprador/CategoriesActivity.java
@@ -1,17 +1,82 @@
 package com.example.tfg.comprador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.tfg.R;
+import com.example.tfg.modelos.Products;
+import com.example.tfg.viewholders.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import io.paperdb.Paper;
 
 public class CategoriesActivity extends AppCompatActivity {
+    private DatabaseReference ProductsRef;
+    private String type = "";
+    private RecyclerView botanicaLista;
+    private Button botanicaBtn;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
         getSupportActionBar().hide();
+
+/*        botanicaBtn = findViewById(R.id.botanica_foto);
+        botanicaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+                FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>().setQuery(ProductsRef.orderByChild("category").equalTo("Botánica"), Products.class).build();
+
+                FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
+                        holder.txtProductoNombre.setText(model.getPname());
+                        holder.txtProductoDescripcion.setText(model.getDescription());
+                        holder.txtNombreTienda.setText(model.getSellerName());
+                        holder.txtProductoPrecio.setText("Precio: " + model.getPrice() + "€");
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(CategoriesActivity.this, ProductDetailsActivity.class);
+                                intent.putExtra("pid", model.getPid());
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_items_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+                botanicaLista.setAdapter(adapter);
+                adapter.startListening();
+            }
+        });*/
     }
 }
+
